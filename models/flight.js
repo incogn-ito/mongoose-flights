6f1ce8f652d33119f86a302f78ebcc14dadd33d2
Ito Aghayere <itohan.aghayere@gmail.com>
2024-08-11 00:29:44 -0700

    Part 2 Ex 1 and 2

--- models/flight.js
+++ models/flight.js
@@ -3,6 +3,16 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+    seat: String,
+    price: {
+        type: Number,
+        min: 0
+    }
+  }, {
+    timestamps: true
+  })
+
 const flightSchema = new Schema ({
     airline: {
         type: String,
@@ -18,6 +28,7 @@ const flightSchema = new Schema ({
         min: 10,
         max: 9999,
     },
+    tickets: [ticketSchema],
     departs: Date
     }, {    
         timestamps: true
